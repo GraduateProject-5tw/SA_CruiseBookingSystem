95b933eeb83fb0563153b0b7533d1f24fd938d2a
ydnas-121 <68885034+ydnas-121@users.noreply.github.com>
2020-12-23 18:43:40 +0800

    Create StopPort.java
    新增

---
+++ StopPort.java
@@ -0,0 +1,80 @@
+package ncu.im3069.demo.app;
+
+import org.json.*;
+
+public class StopPort {
+
+    /** id，會員編號 */
+    private int id;
+
+    /** id，會員編號 */
+    private String name;
+
+    /**
+     * 實例化（Instantiates）一個新的（new）Product 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於新增產品時
+     *
+     * @param id 產品編號
+     */
+	public StopPort(int id) {
+		this.id = id;
+	}
+
+    /**
+     * 實例化（Instantiates）一個新的（new）Product 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於新增產品時
+     *
+     * @param name 產品名稱
+     * @param price 產品價格
+     */
+
+    /**
+     * 實例化（Instantiates）一個新的（new）Product 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於修改產品時
+     *
+     * @param id 編號
+     * @param name 名稱
+     */
+	public StopPort(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+    /**
+     * 取得產品編號
+     *
+     * @return int 回傳產品編號
+     */
+	public int getID() {
+		return this.id;
+	}
+
+    /**
+     * 取得產品名稱
+     *
+     * @return String 回傳產品名稱
+     */
+	public String getName() {
+		return this.name;
+	}
+
+    /**
+     * 取得產品價格
+     *
+     * @return double 回傳產品價格
+     */
+
+    /**
+     * 取得產品資訊
+     *
+     * @return JSONObject 回傳產品資訊
+     */
+	public JSONObject getData() {
+        /** 透過JSONObject將該項產品所需之資料全部進行封裝*/
+        JSONObject jso = new JSONObject();
+        jso.put("id", getID());
+        jso.put("name", getName());
+
+        return jso;
+    }
+}
